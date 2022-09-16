9921ab54d472624d26d47ee6ed292ae7971c2985
bratpeki <katic.peco@gmail.com>
2022-09-16 11:38:27 +0200

    Added a comment to SDL_Event to clear ambiguity

--- src/event.c
+++ src/event.c
@@ -13,6 +13,16 @@ extern SDL_bool stateRunning;
 
 void eventHandle() {
 
+	/*
+	 * SDL_WaitEvent waits for an event to be input
+	 * Therefore, it halts program execution until it gets and input
+	 *
+	 * In case you want the program to run independent of input events
+	 * you might want to use the following:
+	 *
+	 * while (SDL_PollEvent(&event)) { ... }
+	 */
+
 	SDL_WaitEvent(&event);
 
 	switch (event.type) {
